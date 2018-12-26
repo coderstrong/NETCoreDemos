7e143916ecf671a0e98f5862671816378caee33b
Joh Tran <thao31b@gmail.com>
2018-12-26 17:35:54 +0800

    demo redis cache

---
+++ ApiRedisCache/Controllers/ValuesController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Logging;
+
+namespace ApiRedisCache.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly IDistributedCache _distributedCache;
+        private readonly ILogger<ValuesController> _logger;
+        public ValuesController(IDistributedCache distributedCache, ILogger<ValuesController> logger){
+            this._distributedCache = distributedCache;
+            this._logger = logger;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            var cachekey ="test";
+            string data = _distributedCache.GetString(cachekey);
+            if(!string.IsNullOrWhiteSpace(data)){
+                return "Fetched from cache : " + data;
+            }
+            else
+            {
+                data = "Redis ok";
+                _distributedCache.SetString(cachekey, data);
+                return "Added to cache : " + data;
+            }
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- BasicTokenAuthentication/README.md
+++ BasicTokenAuthentication/README.md
@@ -7,4 +7,6 @@ Override other methods to change standard behaviour
 Register the scheme with AddScheme<TOptions, THandler>(string, Action<TOptions>) on the AuthenticationBuilder, which you get by calling AddAuthentication on the service collection
 
 
-Refer: https://joonasw.net/view/creating-auth-scheme-in-aspnet-core-2
+Refer: 
+https://joonasw.net/view/creating-auth-scheme-in-aspnet-core-2
+https://docs.microsoft.com/en-us/dotnet/api/microsoft.aspnetcore.authentication?view=aspnetcore-2.1
